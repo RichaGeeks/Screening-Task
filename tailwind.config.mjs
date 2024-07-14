875f586b1b24e2e40ed0244eec7cf72c0c3bbd89
RichaGeeks <richasuryawanshi1505@gmail.com>
2024-07-14 15:06:36 +0530

    commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			dropShadow: {
+				'3xl': '0 35px 35px rgba(255, 239, 84, 1)'
+			}
+		},
 	},
 	plugins: [],
 }
